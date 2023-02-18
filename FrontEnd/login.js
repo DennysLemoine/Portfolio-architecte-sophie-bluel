b4a0f5545e3b7e8f30a5b968109ecf12c371c45c
YUHI <dennys.lemoine.1@gmail.com>
2023-02-18 12:09:13 +0100

    feat: end of login service

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,36 +1,31 @@
+// RECUPERATION D'ELEMENT HTML
 const logCase = document.querySelector("#userlogin");
-// const logCase = document.getElementById('userlogin');
 const passCase = document.querySelector("#userpass");
 const button = document.querySelector('.btn_Tologin');
-const loginEmpty = document.getElementById("empty_text");
-const passEmpty = document.getElementById("error_login");
-// const form = document.querySelector(".form_login");
-// const emptyContainer = document.querySelector("#empty_text");
-// const errorContainer = document.querySelector("#error_login");
-// const submit = document.querySelector(".btn_Tologin");
 
-// console.log(form);
+// CREATION MESSAGES D'ERREURS + PLACEMENT
+const emptyMessage = document.createElement('p');
+emptyMessage.setAttribute('id', 'empty_text');
+const errorLog = document.createElement('p');
+errorLog.setAttribute('id', 'error_login');
+
+const errorContainer = document.querySelector(".div_form");
+errorContainer.append(emptyMessage, errorLog);
+
+// CREATION COMPORTEMENT BOUTON SE CONNECTER
 button.addEventListener("click", (e) => {
     e.preventDefault();
     const urlApi = "http://localhost:5678/api/users/login";
 
     if (logCase.value.length === 0 || passCase.value.length === 0) {
 
-        let errorMessage = document.createElement('p');
-        errorMessage.setAttribute('id', 'empty_text');
-        errorMessage.innerText = "Veuillez renseigner les champs."
+        emptyMessage.innerText = "Veuillez renseigner les champs.";
+        emptyMessage.style.display = "block";
+        errorLog.style.display = "none";
 
-        let test = document.querySelectorAll('.div_form')[0];
-
-        test.appendChild(errorMessage);
-
-
-        /*
-                loginEmpty.style.display = "block";
-                passEmpty.style.display = "none";*/
     } else {
 
-
+        // APPEL API/POST POUR VERIFIER L'AUTHENTIFICATION
         fetch(urlApi, {
             method: 'POST',
             headers: {
@@ -45,38 +40,26 @@ button.addEventListener("click", (e) => {
             .then((data) => {
                 console.log(data);
 
-                if (data.message) {
+                if (data.error) {
 
-                    let errorMessage = document.createElement('p');
-                    errorMessage.setAttribute('id', 'empty_text');
-                    errorMessage.innerText = "Email et/ou Mot de passe incorrect(s)."
-
-                    let test = document.querySelectorAll('.div_form')[0];
-                    console.log(test);
-                    test.appendChild(errorMessage);
-
+                    errorLog.innerText = "Email et/ou Mot de passe incorrect(s).";
+                    errorLog.style.display = "block";
+                    emptyMessage.style.display = "none";
 
                 } else {
-                    console.log(data);
+                    // console.log(data);
+                    errorLog.style.display = "none";
+                    emptyMessage.style.display = "none";
 
+                    //ENREGISTRER LE TOKEN DANS LE LOCALSTORAGE POUR APRES
                     localStorage.setItem('token', data.token);
 
-                    //document.location.href ='index.html';
-
-
-                    /*
                     window.open(
 
-                        "logged.html"
+                        "index.html"
                     );
-                    passEmpty.style.display = "none";
-                    loginEmpty.style.display = "none";
-
-                     */
                 }
             })
             .catch(error => console.error(error));
-
     }
-
 })
